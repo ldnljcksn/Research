0f39588442315fdea2bdcb4bd23e7a9421e7962b
ldnljcksn <ldnljcksn@gmail.com>
2020-05-01 15:17:45 -0400

    new start

---
+++ Simulating the Brain/src/node.py
@@ -0,0 +1,9 @@
+class Node:
+	def __init__(self, value):
+		self.value = value
+
+	def print_value(self):
+		print("Node's value is " + self.value)
+
+	def set_value(self, value):
+		self.value = value
